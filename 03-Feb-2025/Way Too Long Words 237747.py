5e777b8ab294e94e39acbfb1bf299a23b02ba9df
Aschalew Abayneh <abayasche0@gmail.com>
2025-02-03 23:56:34 +0300

    Created 'Way Too Long Words' by A2SV Hub

---
+++ 03-Feb-2025/Way Too Long Words 237747.py
@@ -0,0 +1,9 @@
+# Problem: Way Too Long Words - https://codeforces.com/problemset/problem/71/A
+
+t=int(input())
+for i in range(t):
+    st=input()
+    if len(st) <= 10:
+        print(st)
+    else:
+        print(st[0]+ str((len(st)-2))+st[-1])
